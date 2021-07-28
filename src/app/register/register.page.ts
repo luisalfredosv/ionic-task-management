c1c187bf429e312ecdd17bf030f709c21f0b4e7b
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-07-28 14:43:09 -0400

    Page register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  usuario = this.form.group({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+  });
 
+  constructor(
+    private form: FormBuilder, 
+    private apiServ: ApiService,
+    public toasterController: ToastController,
+    public alertCtrl:AlertController,
+    private router: Router
+    ) {}
+    
   ngOnInit() {
   }
+
+
 
 }
