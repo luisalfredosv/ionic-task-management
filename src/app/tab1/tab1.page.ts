3b54a5ef0ca7f071a22f67dc0b0db3eb8db56db1
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-07-28 15:43:29 -0400

    List task tab1.page.html and tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private apiServ: ApiService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.getTask();
+  }
 
+  tasks: Task[] = [];
+  getTask() {
+    return this.apiServ.getAllTask().subscribe(async (data) => {
+      this.tasks = data
+      console.log('data', data);
+      
+    });
+  }
+}
+
+interface Task{
+  description: string;
+  id: string;
+  status: string;
+  title: string;
 }
